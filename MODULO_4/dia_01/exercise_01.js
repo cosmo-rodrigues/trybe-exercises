42b1de15ebfa37fa2203d9feb21662d2d8761819
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-08-28 00:59:19 -0300

    Exercios 1 a 6 de JS

---
+++ MODULO_4/dia_01/exercise_01.js
@@ -0,0 +1,26 @@
+//Operações báicas com JS - Bloco 4 - Dia 1
+
+let a = 5;
+let b = 5;
+let result_1 = a + b;
+console.log(result_1);
+
+let c = 5;
+let d = 5;
+let result_2 = a * b;
+console.log(result_2);
+
+let e = 5;
+let f = 5;
+let result_3 = a / b;
+console.log(result_3);
+
+let g = 5;
+let h = 5;
+let result_4 = a - b;
+console.log(result_4);
+
+let i = 5;
+let j = 5;
+let result_5 = a % b;
+console.log(result_5);
---
+++ MODULO_4/dia_01/exercise_03.js
@@ -0,0 +1,11 @@
+let number_01 = 5;
+let number_02 = 10;
+let number_03 = 7
+if(number_01>number_02&&number_01>number_03){
+console.log(number_01)
+} else if(number_02>number_01&&number_02>number_03){
+    console.log(number_02)
+}
+    else{
+     console.log(number_03)
+}
