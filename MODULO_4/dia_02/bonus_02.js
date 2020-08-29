cce689ea38d64efb0976f21039e806db3ffc54fb
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-08-29 15:03:09 -0300

    Exercicios bonus finalizados

---
+++ MODULO_4/dia_02/bonus_02.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let bigger = 0;
+for(let count1 in numbers){
+    for(let count2 in numbers){
+    if(numbers[count1]>numbers[count2]){
+        bigger=numbers[count1];
+        numbers[count1]=numbers[count2];
+        numbers[count2]=bigger;
+        }
+    }
+}
+console.log(numbers);
