df0f42e7e0c1914a5153c56701ba6b32cbb995c4
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-09-10 00:56:22 -0300

    Atualizar eventos de click

---
+++ MODULO_4/dia_04/index.js
@@ -0,0 +1,12 @@
+// function soma(number1,number2){
+//     return number1+number2;
+// }
+// console.log(soma(12,13));
+
+// let soma = (number1,number2)=>{
+//     return number1 + number2;
+// }
+// console.log(soma(12,10));
+
+let soma = (number1,number2)=>number1 + number2;
+console.log(soma(12,10));
