9807274da6462966785356a0aad14f326ecb8814
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-12-02 17:59:06 -0300

    Tic tac toe tests

--- desenvolvimento_front-end/MODULO_15/dia_01/tic-tac-toe/src/App.test.js
+++ desenvolvimento_front-end/MODULO_15/dia_01/tic-tac-toe/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
- const { getByText } = render(<App />);
-  const linkElement = getByText('Tic Tac Toe');
-  expect(linkElement).toBeInTheDocument();
+describe('Verify all compenets in screen', () => {
+  it('Shoul render App componete correctly', () => {
+    render(<App />);
+  });
 });
