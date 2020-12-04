fb25e71d31637f272758702d1fd0cb962a8f55d1
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-12-03 22:52:01 -0300

    Dia 15.1 tests

---
+++ desenvolvimento_front-end/MODULO_15/dia_01/testes-react/src/App.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+describe('Check all inputs and lables', () => {
+  it('Should have a lable Email with type = \'email\'', () => {
+    render(<App />);
+    const emailInput = screen.getByLabelText(/Email/i);
+    expect(emailInput).toBeInTheDocument();
+    expect(emailInput.type).toBe('email');
+  });
+  it('Should have 2 buttons', () => {
+    render(<App />);
+    const buttons = screen.getAllByRole('button');
+    expect(buttons).toHaveLength(2);
+    expect(buttons[0].value).toBe('Enviar');
+    expect(buttons[1].value).toBe('Voltar')
+  });
+})
